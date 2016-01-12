be5a42e857f04f06ffcbf6449da3f01090448228
Angelina <angelinaprod@gmail.com>
2016-01-11 19:36:57 -0800

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,5 +84,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+ #--------------- MICROPOST ------------------#
+
+test "associated microposts should be destroyed" do  
+  @user.save
+  @user.microposts.create!(content: "Lorem ipsum")
+  assert_difference 'Micropost.count', -1 do 
+    @user.destroy 
+  end
+end
+
+
 
 end
+
+
+
+
+
+
+
+
